db94c85127389fccb9d345265f0a15e77d6efb38
Nawaf0000 <129116906+Nawaf0000@users.noreply.github.com>
2023-05-08 04:32:01 -0700

    Create segment.h

---
+++ segment.h
@@ -0,0 +1,8 @@
+#ifndef SEGMENT_H
+#define SEGMENT_H
+void initsegment();
+void turnOnSEGMENT(unsigned int segment);
+void turnOffSEGMENT(unsigned int segment);
+void runSEGMENT(unsigned int segment);
+#endif
+
